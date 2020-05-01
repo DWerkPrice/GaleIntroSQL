c2a073f012bd25f0dbb7e50daf647282ba9e222a
David Price <dwerkprice@gmail.com>
2020-05-01 00:29:11 -0400

    adds lesson 3

---
+++ SQLINTROCH3.sql
@@ -0,0 +1,34 @@
+/*SELECT PlanName, PlanID, PlanDEscription, PlanPrice, DiscountedPrice 
+FROM ServicePlans
+ORDER BY PlanName;
+
+SELECT PlanName, PlanID, PlanDEscription, PlanPrice, DiscountedPrice 
+FROM ServicePlans
+ORDER BY 1;
+
+SELECT PlanPrice, PLanName FROM ServicePlans
+ORDER BY PlanPrice, PlanName;
+
+SELECT PlanName, PlanPrice, DiscountedPrice
+FROM ServicePlans
+ORDER BY PlanName DESC;
+
+
+SELECT PlanID, PlanName,PlanDescription
+FROM ServicePlans
+WHERE PlanID IN  ('W1001','D2202','D1003','L2002')
+ORDER BY PlanName;
+
+
+SELECT PlanID, PlanName, PlanDescription
+FROM ServicePlans
+WHERE PlanID NOT IN ('W1001','D2202','D1003','L2002')
+ORDER BY PlanName;
+
+
+
+SELECT * FROM ServicePlans WHERE PlanDescription Like '%min%'; */
+
+SELECT * FROM ServicePlans
+WHERE PlanID LIKE "D"
++9,
